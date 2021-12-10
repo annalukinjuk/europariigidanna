2d44ae9bf832eb7fcb8c4996e8b193ca341f026e
opilane <opilane@HP-CZC6297BBM>
2021-12-10 13:17:31 +0200

    1

--- europariigidanna.py
+++ europariigidanna.py
@@ -1,6 +1,5 @@
 from module1 import*
 
-pealinn={}
 pealinn=dict()
 pealinn["Belgia"] = "Brüssel"
 pealinn["Saksamaa"] = "Berliin"
@@ -30,11 +29,34 @@ pealinn["Rumeenia"] = "Bukarest"
 pealinn["Horvaatia"] = "Zagreb"
 pealinn= ["Brüssel", "Berliin","Pariis","Rooma","Luxembourg","Amsterdam","Kopenhaagen","Dublin","Ateena","Lissabon","Madrid","Helsingi","Viin", "Stockholm","Tallinn", "Riia","Valletta","Varssavi","Bratislava",   ]
 europaliit=["Belgia","Saksamaa","Prantsusmaa","Itaalia","Luksemburg","Holland","Taani","Iirimaa","Kreeka","Portugal","Hispaania","Soome","Austria","Rootsi","Eesti","Läti","Malta","Poola","Slovakkia","Sloveenia","Tšehhi","Küpros","Ungari","Bulgaaria","Rumeenia","Horvaatia"]
+
+country=input("siseta riik eu liitust:   ")
 for country in europaliit:
- if country in pealinn:
-    print("linnude pealinn on" + country + ': ' + Capitals[country])
- else:
-    print( + country)
+    if country in europaliit:
+        print(f"linnude pealinn on {country},{pealinn}")
+    else:
+        print(country)
+
+valik=input("\n1 - siseta riik \n2 - siseta listis eu liitu linnud ja pealinn. \n3 - listis viga soritama \n4 - kas oled õige?.")
+if valik=="1":
+    try:
 
 while 1:
-    print("\n1 - siseta riik \n2 - siseta listis eu liitu linnud ja pealinn. \n3 - Исравить ошибку в списке. \n4 - Проверь себя.")
+    print("\nsiseta riik - A \nsiseta listis eu liitu linnud ja pealinn - B \nlistis viga soritama - C \nkas oled õige? - D")
+    valik=input("vajuta nuppu:  ")
+
+    if valik()=="A":
+        country=input("siseta riik eu liitust:   ")
+
+    elif read_key()=="B":
+      0000,0
+        min_palk, kellel=minimum(palgad,inimesed)
+        print("minimaalne palk ==> ", min_palk, " Kellel ==> ",kellel)
+
+    elif read_key()=="C":
+        max_palk, kellel=maksimum(palgad,inimesed)
+        print("maksimaalne palk ==> ", max_palk, " Kellel ==> ",kellel)
+
+    elif read_key()=="V":
+        lisa(palgad,inimesed)
+        print("siseta info  ", ans, "kellel==>")
