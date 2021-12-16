42bdd5521173448c06c6b9d92021dd4a785e69ef
opilane <opilane@HP-CZC6297BBM>
2021-12-16 12:29:35 +0200

    22we3

--- europariigidanna.py
+++ europariigidanna.py
@@ -1,51 +1,86 @@
-from module1 import*
+from random import*
+from module2 import*
+Capitals=dict()
+Capitals["Estonia"]="Tallinn"
+Capitals["Albania"]="Tirana"
+Capitals["Belgium"]="Brussels"
+Capitals["Czechia"]="Prague"
+Capitals["Poland"]="Warsaw"
+Capitals["Portugal"]="Lisbon"
+Capitals["Finland"]="Helsinki"
+Capitals["France"]="Paris"
+Capitals["Germany"]="Berlin"
+Capitals["Sweden"]="Stockholm"
+Capitals["Spain"]="Madrid"
+Capitals["Serbia"]="Belgrade"
+Capitals["Norway"]="Oslo"
+Capitals["Moldova"]="Chisinau"
+Capitals["Greece"]="Athens"
+Capitals["Bulgaria"]="Sofia"
+Capitals["Austria"]="Vienna"
+Capitals["Switzerland"]="Bern"
+Countries=["Estonia","Albania","Belgium","Czechia","Poland","Portugal","Finland","France","Germany","Sweden","Spain","Serbia","Norway","Moldova","Greece","Bulgaria","Austria","Switzerland"]
+valik=input("\n siseta riik ja vaata linnud - 1,\n Test - 2")
+if valik=="1":
 
-pealinn=dict()
-pealinn["Belgia"] = "Brüssel"
-pealinn["Saksamaa"] = "Berliin"
-pealinn["Prantsusmaa"] = "Pariis"
-pealinn["Itaalia"] = "Rooma"
-pealinn["Luksemburg"] = "Luxembourg"
-pealinn["Holland"] = "Amsterdam"
-pealinn["Taani"] = "Kopenhaagen"
-pealinn["Iirimaa"] = "Dublin"
-pealinn["Kreeka"] = "Ateena"
-pealinn["Portugal"] = "Lissabon"
-pealinn["Hispaania"] = "Madrid"
-pealinn["Soome"] = "Helsingi"
-pealinn["Austria"] = "Viin"
-pealinn["Rootsi"] = "Stockholm"
-pealinn["Eesti"] = "Tallinn"
-pealinn["Läti"] = "Riia"
-pealinn["Malta"] = "Valletta"
-pealinn["Poola"] = "Varssavi"
-pealinn["Slovakkia"] = "Bratislava"
-pealinn["Sloveenia"] = "Ljubljana"
-pealinn["Tšehhi"] = "Praha"
-pealinn["Ungari"] = "Budapest"
-pealinn["Küpros"] = "Nikosia"
-pealinn["Bulgaaria"] = "Sofia"
-pealinn["Rumeenia"] = "Bukarest"
-pealinn["Horvaatia"] = "Zagreb"
-pealinn= ["Brüssel", "Berliin","Pariis","Rooma","Luxembourg","Amsterdam","Kopenhaagen","Dublin","Ateena","Lissabon","Madrid","Helsingi","Viin", "Stockholm","Tallinn", "Riia","Valletta","Varssavi","Bratislava",   ]
-europaliit=["Belgia","Saksamaa","Prantsusmaa","Itaalia","Luksemburg","Holland","Taani","Iirimaa","Kreeka","Portugal","Hispaania","Soome","Austria","Rootsi","Eesti","Läti","Malta","Poola","Slovakkia","Sloveenia","Tšehhi","Küpros","Ungari","Bulgaaria","Rumeenia","Horvaatia"]
-
-country=input("siseta riik eu liitust:   ")
-for country in europaliit:
-    if country in europaliit:
-        print(f"linnude pealinn on {country},{pealinn}")
-    else:
-        print(country)
-
-
-while 1:
-    print("\nsiseta riik - A \nsiseta listis eu liitu linnud ja pealinn - B \nlistis viga soritama - C \nkas oled õige? - D")
-    valik=input("vajuta nuppu:  ")
-    if valik()=="A":
-        country=input("siseta riik eu liitust:   ")
-        for country in europaliit:
-            if country in europaliit:
-                print(f"linnude pealinn on {country},{pealinn}")
+    for country in Countries:
+        country=input("Введите страну: ")
+        if country in Capitals:
+            print("Столица страны "+country+": " +Capitals[country])
         else:
+            print("В базе данных нет страны с названием " +country)
+            v=input("Хотите внести " +country+ " в базу данных?Да или Нет? ")
+            if v=="Да":
+                ca=input("Введите столицу страны " +country+": ")
+                Capitals.update({country: ca})
+                p=input("Возможно в базе данных ошибка, хотите исправить её? Да или Нет? ")
+                if p=="Нет":
+                    print("Хорошо")
+                if p=="Да":
+                    o=input("Введите правильно страну: ")
+                    l=input("Введите правильно столицу: ")
+                    Capitals.pop(country)
+                    Capitals.update({o: l})
+            if v=="Нет":
+                print("Хорошо")
+        d=input("Хотите ли вы начать проговаривание слов для самостоятельного изучения? Да или Нет? ")
+        if d=="Да":
+            sonastik()
+        if d=="Нет":
+            print("Хорошо")
+if valik=="2":
+        Countries.sort()
+        Countries.reverse()
+        m=0
+        for i in range(10):
+            country=str(choice(Countries))
             print(country)
+            st=input("Введите столицу: ")
+            if st==Capitals[country]:
+                print("Правильно!")
+                punkt+=1
+            else:
+                print("Неправильно!")
+        if punkt==0:
+            print("0%")
+        elif punkt==1:
+            print("10%")
+        elif punkt==2:
+            print("20%")
+        elif punkt==3:
+            print("30%")
+        elif punkt==4:
+            print("40%")
+        elif punkt==5:
+            print("50%")
+        elif punkt==6:
+            print("60%")
+        elif punkt==7:
+            print("70%")
+        elif punkt==8:
+            print("80%")
+        elif punkt==9:
+            print("90%")
+        elif punkt==10:
+            print("100%")
 
--- module2.py
+++ module2.py
@@ -1,48 +1,21 @@
 from gtts import gTTS
-import os
-sõnastik={}
-riigid=[]
-linnad=[]
-file=open("riigid_pealinnad.txt", 'r')
-for line in file:
-    k, v=line.strip().split('-') #riik:pealinn
-    sonastik[k.strip()]=v.strip
-    riigid.append(k)
-    linnad.append(sonastik[k.strip()])
-file.close()
-print(sonastik)
-print("riigid:  ")
-print(riigid)
-print("pealinnad:  ")
-print(linnad)
-s=gTTS(text=linnad[0],lang='et',slow=True).save("heli.mp3")
-os.system("heli.mp3")
-a=input()
-
-def europaliit(d:dict, v:int):
-    keys_list=(list(d.keys()))
-    values_list=(list(d.values()))
-    if v=="1":
-        mas=[]
-        capital=(input("Введи столицу: ")).capitalize()
-        capital.title()
-        for i in values_list:
-            if i==capital:
-                for i in range(len(values_list)):
-                    if values_list[i]==capital:
-                        mas.append(int(i))
-                        print("Страна -", keys_list[i],"<-->", "Cтолица -", values_list[i])
-    else:
-        country=(input("Введите страну: ")).capitalize()
-        a=d.get(country)
-        print("Страна -", country ,"<-->", "Cтолица -", a)
-    return
-
-
-def new_key_value(d:dict):
-    new={}
-    country=(input("Введите страну: ")).capitalize()
-    capital=(input("Введите страну: ")).capitalize()
-    new={country:capital}
-
-    return d,new
+import o
+def sonastik():
+        sonastik={}
+        countries=[]
+        capitals=[]
+        file=open("countries-.txt","r")
+        for line in file:
+            k, v=line.strip().split("-")
+            sonastik[k.strip()]=v.strip()
+            countries.append(k)
+            capitals.append(sonastik[k.strip()])
+        file.close()
+        print(sonastik)
+        print("Countries: ")
+        print(countries)
+        print("Capitals:")
+        print(capitals)
+        s=gTTS(text=capitals[6],lang="est",slow=False).save("countries.mp3")
+        os.system("countries.mp3")
+        a=input()
